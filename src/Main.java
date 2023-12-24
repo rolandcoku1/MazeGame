0ae0adc23abe3d5d7435628af63673208579ae6e
RolandCoku <roland.coku@fti.edu.al>
2023-12-24 17:10:55 +0100

    Initial setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rrushi Kices");
-        System.out.println("msmnsns");
-        System.out.println("Te befte mire");
+
     }
 }
