8be16a7fe3bb98e101ff68d62a0792ce987af886
RolandCoku <roland.coku@fti.edu.al>
2023-12-24 17:21:34 +0100

    Player class attributes

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,20 @@
 public class Player {
+    private int playerPositionY;
+    private int playerPositionX;
+    private int treasureCount;
+    private int pointsEarned;
 
+    public Player(){
+        this.playerPositionX = 0;
+        this.playerPositionY = 0;
+        this.pointsEarned = 0;
+        this.treasureCount = 0;
+    }
+
+    public Player(int playerPositionY, int playerPositionX, int treasureCount, int pointsEarned){
+        this.playerPositionY = playerPositionY;
+        this.playerPositionX = playerPositionX;
+        this.treasureCount = treasureCount;
+        this.pointsEarned = pointsEarned;
+    }
 }
