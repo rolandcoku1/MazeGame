ba74c4787d2b3f3a04f60a1cf549c68a80b685d6
ReiLamaj1 <154746091+ReiLamaj1@users.noreply.github.com>
2023-12-24 16:33:38 +0100

    //

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bsahbsha");
+        System.out.println("Rrushi Kices");
+        System.out.println("msmnsns");
     }
 }
